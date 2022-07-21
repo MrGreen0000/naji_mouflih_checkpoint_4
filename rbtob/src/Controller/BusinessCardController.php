6ac2b44bd613e28305dac5c1b42f03ebb823d2f6
MrGreen0000 <naji.mouflih@gmail.com>
2022-07-21 17:25:13 +0200

    update my project

--- rbtob/src/Controller/BusinessCardController.php
+++ rbtob/src/Controller/BusinessCardController.php
@@ -4,21 +4,30 @@ namespace App\Controller;
 
 use App\Form\BusinessCardType;
 use App\Entity\BusinessCard;
+use App\Repository\BusinessCardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 
 class BusinessCardController extends AbstractController
 {
     #[Route('/new/', name: 'new')]
-    public function new(): Response
+    public function new(Request $request, BusinessCardRepository $businessCardRepository): Response
     {
         $businessCard = new BusinessCard();
 
         // Create the form, linked with $
         $form = $this->createForm(BusinessCardType::class, $businessCard);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $businessCardRepository->add($businessCard, true);
+        
+            return $this->redirectToRoute('app_home');
+        }
         
         // Render the form (best practice)
         return $this->renderForm('businessCard/new.html.twig', [
